29c29e0cf780d6e881f51513a9d296f3870c4ada
Luke Vance <luke@cloud-elements.com>
2018-09-20 10:31:57 -0600

    moved league summary table into main view

--- src/MainContentContainer.jsx
+++ src/MainContentContainer.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-import DataTableContainer from './DataTableContainer';
+import LeagueSummaryTableContainer from './js/components/LeagueSummaryTable.container';
 
 const styles = theme => ({
   root: {
@@ -19,15 +14,40 @@ const styles = theme => ({
   }
 });
 
-function MainContent(props) {
-  const { classes } = props;
+class MainContent extends React.Component {
+  // (props) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leagueId: '286565'
+    }
+  }
 
-  return (
-    <div className={classes.root}>
-        <h1>Your League Name Here</h1>
-        <DataTableContainer />
-    </div>
-  );
+  componentWillMount(){
+    const getleagueData = async (leagueId) => {
+      let url = 'http://games.espn.com/ffl/api/v2/teams?leagueId=' + leagueId;
+      const res = await fetch(url);
+      const json = await res.json();
+      // save teams to current state
+      this.setState({
+        leagueData: json.teams
+      });
+    }
+   getleagueData(this.state.leagueId);
+  }
+
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Typography variant="title"> Your League Name </Typography>
+        {/* <h1>Your League Name Here</h1> */}
+        {/* <DataTableContainer /> */}
+        <LeagueSummaryTableContainer teams={this.state.teams} />
+      </div>
+    );
+  }
 }
 
 MainContent.propTypes = {
--- src/js/components/LeagueSummaryTable.container.jsx
+++ src/js/components/LeagueSummaryTable.container.jsx
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+    root: {
+      width: '95%',
+      marginTop: theme.spacing.unit * 3,
+      // marginLeft: theme.spacing.unit * 3,
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+  });
 
 class LeagueSummaryTableContainer extends Component {
     // Make API call to get league info using leagueId
 
     // After retrieving team info, render Table with rows for each team
-
+    
 
     render(){
+        const {classes} = this.props;
+
         return (
             <Paper className={classes.root}>
             <Table className={classes.table}>
@@ -41,3 +63,9 @@ class LeagueSummaryTableContainer extends Component {
         )
     }
 }
+
+LeagueSummaryTableContainer.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LeagueSummaryTableContainer);
