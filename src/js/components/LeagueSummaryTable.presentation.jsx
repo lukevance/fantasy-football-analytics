05046175ad305d1508287642f07d833bbfa077cf
Luke Vance <luke@cloud-elements.com>
2018-09-20 11:54:14 -0600

    styling drives me nuts

--- src/js/components/LeagueSummaryTable.presentation.jsx
+++ src/js/components/LeagueSummaryTable.presentation.jsx
@@ -1,7 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -13,18 +16,27 @@ import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
     root: {
-      width: '95%',
-      marginTop: theme.spacing.unit * 3,
-      // marginLeft: theme.spacing.unit * 3,
-      overflowX: 'auto',
+        width: '95%',
+        marginTop: theme.spacing.unit * 3,
+        // marginLeft: theme.spacing.unit * 3,
+        overflowX: 'auto',
     },
     table: {
-      minWidth: 700,
+        minWidth: 700,
     },
-  });
+    avatar: {
+        margin: 7,
+        // width: 60,
+        // height: 60,
+    },
+    icon: {
+        margin: theme.spacing.unit * 0.2,
+        fontSize: 12
+      },
+});
 
 class LeagueSummaryTable extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         // this.state = {
         //     teams: this.props.teams
@@ -32,62 +44,68 @@ class LeagueSummaryTable extends Component {
     }
 
     // After retrieving team info, render Table with rows for each team
-    
 
-    render(){
-        const {classes, teams, leagueId} = this.props;
+
+    render() {
+        const { classes, teams, leagueId } = this.props;
 
         return (
             <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                <TableRow>
-                <TableCell
-                        sortDirection={false}
-                    >
-                        <Tooltip
-                            title="Sort"
-                            placement='bottom-start'
-                            enterDelay={300}
-                        >
-                        <TableSortLabel
-                            active={true}
-                            direction='desc'
-                            // onClick={this.createSortHandler(row.id)}
-                        >
-                            Team name
-                        </TableSortLabel>
-                        </Tooltip>
-                    </TableCell>
-                    {/* <TableCell>Team name</TableCell> */}
-                    <TableCell >Owner</TableCell>
-                    <TableCell numeric>Wins</TableCell>
-                    <TableCell numeric>Losses</TableCell>
-                    <TableCell numeric>Points Scored</TableCell>
-                    <TableCell numeric>Waiver Order</TableCell>
-                    <TableCell numeric>Acquisitions</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {teams.map(team => {
-                    return (
-                    <TableRow key={team.teamId}>
-                        <TableCell component="th" scope="row">
-                            <Button href={`http://games.espn.com/ffl/clubhouse?leagueId=${leagueId}&teamId=${team.teamId}&seasonId=2018`} className={classes.button}>
-                                {team.teamLocation + " " + team.teamNickname}
-                            </Button>
-                        </TableCell>
-                        <TableCell>{team.owners[0].firstName + " " + team.owners[0].lastName}</TableCell>
-                        <TableCell numeric>{team.record.overallWins}</TableCell>
-                        <TableCell numeric>{team.record.overallLosses}</TableCell>
-                        <TableCell numeric>{team.record.pointsFor}</TableCell>
-                        <TableCell numeric>{team.waiverRank}</TableCell>
-                        <TableCell numeric>{team.teamTransactions.overallAcquisitionTotal}</TableCell>
-                    </TableRow>
-                    );
-                })}
-                </TableBody>
-            </Table>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell />
+                            <TableCell sortDirection={false}>
+                                <Tooltip
+                                    title="Sort"
+                                    placement='bottom-start'
+                                    enterDelay={300}
+                                >
+                                    <TableSortLabel
+                                        active={true}
+                                        direction='desc'
+                                    >
+                                        Team name
+                                    </TableSortLabel>
+                                </Tooltip>
+                            </TableCell>
+                            {/* <TableCell>Team name</TableCell> */}
+                            <TableCell >Owner</TableCell>
+                            <TableCell numeric>Wins</TableCell>
+                            <TableCell numeric>Losses</TableCell>
+                            <TableCell numeric>Points Scored</TableCell>
+                            <TableCell numeric>Waiver Order</TableCell>
+                            <TableCell numeric>Acquisitions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {teams.map(team => {
+                            return (
+                                <TableRow key={team.teamId}>
+                                    <TableCell component="th" scope="row">
+                                        <Avatar
+                                            alt="Team Logo"
+                                            src={team.logoUrl}
+                                            className={classes.avatar}
+                                        />
+                                    </TableCell>
+                                    <TableCell>
+                                            {team.teamLocation + " " + team.teamNickname}
+                                            <IconButton className={classes.button} aria-label="Team Link" href={`http://games.espn.com/ffl/clubhouse?leagueId=${leagueId}&teamId=${team.teamId}&seasonId=2018`} className={classes.button}>
+                                                <OpenInNewIcon className={classes.icon}/>
+                                            </IconButton>
+                                    </TableCell>
+                                    <TableCell>{team.owners[0].firstName + " " + team.owners[0].lastName}</TableCell>
+                                    <TableCell numeric>{team.record.overallWins}</TableCell>
+                                    <TableCell numeric>{team.record.overallLosses}</TableCell>
+                                    <TableCell numeric>{team.record.pointsFor}</TableCell>
+                                    <TableCell numeric>{team.waiverRank}</TableCell>
+                                    <TableCell numeric>{team.teamTransactions.overallAcquisitionTotal}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
             </Paper>
         )
     }
